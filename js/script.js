a4e72375ac4c015d079bb98b4b3582bbbd761bac
Luca <lucafagiolini@live.it>
2024-02-01 23:22:49 +0100

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
 for (let i = 1; i <= 100; i++) {
-
-    if (i % 3 === 0)
+    // se il numero è divisibile per 3 e 5 allora stampa FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+    // se il numero è divisibile per 3 allora stampa Fizz
+    else if (i % 3 === 0) {
         console.log("Fizz")
-
-
-    else if (i % 5 === 0)
+    }
+    // se il numero è divisibile per 5 allora stampa Buzz
+    else if (i % 5 === 0) {
         console.log("Buzz")
-
+    }
+    // altrimenti stampa il valore di (i)
     else {
         console.log(i)
     }
-
-    if (i % 3 === 0 && i % 5 === 0)
-        console.log("FizzBuzz")
-
 }
 
 
